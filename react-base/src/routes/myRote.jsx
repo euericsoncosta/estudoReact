1c2ae52cefd97f4ddfe5f8356d305a061777a02e
Ericson Costa <costaericson32@gmail.com>
2024-06-20 16:17:52 -0300

    fimnalmente

--- react-base/src/routes/myRote.jsx
+++ react-base/src/routes/myRote.jsx
@@ -1,3 +1,29 @@
-import { Route, Redierct } from "react-router-dom";
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types'; // Corrected import of PropTypes
 
-const myRoute = ({ component: Component, isClosed, ...rest }) => {};
+const MyRoute = ({ component: Component, isClosed, ...rest }) => {
+  const isLoggedIn = false;
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { prevPath: rest.location.pathname },
+        }}
+      />
+    );
+  }
+  return <Route {...rest} component={Component} />; // Corrected usage of Route component prop
+};
+
+MyRoute.defaultProps = {
+  isClosed: false,
+};
+
+MyRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.elementType, PropTypes.func])
+    .isRequired, // Corrected PropTypes for component
+  isClosed: PropTypes.bool,
+};
+
+export default MyRoute;
